4a6efb119241ba56b2145f2af7d0c601ce6c5135
siamtalukder484 <siamtalukder484@gmail.com>
2021-07-06 00:31:58 +0600

    third

--- js/custom.js
+++ js/custom.js
@@ -170,3 +170,40 @@ $('.brand-slider').slick({
 
 
 });
+
+//Bannar Type JS Part 
+$(function(){
+  $(".typed").typed({
+		strings: ["Designer", "Developers", "Programmer", "Fullstack"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 150,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 50,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: Infinity,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
